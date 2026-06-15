f83dd130c137c094caac443c0821e23ef462bae3
Alfee Khan <alfeekhan20@gmail.com>
2026-06-15 19:50:28 +0530

    Files added

---
+++ NodeJS/script.js
@@ -0,0 +1,26 @@
+// let args = process.args;
+
+// for (let i = 0; i < args.length; i++) {
+//     console.log("hello & welcome to ", args[i]);
+// }
+
+// const math = require("./math");
+// console.log(math.sum(2,2));
+// console.log(math.PI);
+
+
+// const info = require("./Fruits");
+
+// console.log(info[0].name);
+
+
+//NODE PACKAGE MANAGER
+//NPM => LIBRARY OF PACKAGAEES
+//COMMAND LINE TOO
+
+
+import{sum , PI} from "./math.js";
+
+console.log(sum(1,2));
+console.log(PI);
+
